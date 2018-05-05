226da324d1cf5b9855f9c5dc6dee4400195ec8ab
Irma <irblancofdz@gmail.com>
2018-05-06 00:52:26 +0200

    Refactor for in ChararactersRepo to map and add getter

--- app/src/main/java/com/example/irmablanco/appofthrones/CharactersRepo.kt
+++ app/src/main/java/com/example/irmablanco/appofthrones/CharactersRepo.kt
@@ -1,32 +1,40 @@
 package com.example.irmablanco.appofthrones
 
 object CharactersRepo {
-    var character: MutableList<Character> = mutableListOf()
+    val character: MutableList<Character> = mutableListOf()
+
+    get() {
+        if (field.isEmpty())
+            field.addAll(dummyCharacters())
+
+        return  field
+    }
 
     private  fun dummyCharacters(): MutableList<Character> {
-        val dummies: MutableList<Character> = mutableListOf()
 
-        for (index in 1..10) {
-            val character : Character =  Character(
-                    name = "Personaje ${index}",
-                    title = "Título ${index}",
-                    born = "Nació en ${index}",
-                    actor = "Actor ${index}",
-                    quote = "Frase ${index}",
-                    father = "Padre ${index}",
-                    mother = "Madre ${index}",
-                    spouse = "Esposa ${index}",
-                    house = House(
-                            name = "Casa ${index}",
-                            region = "Region ${index}",
-                            words = "Lema ${index}"
-                    )
+        return (1..10).map {
 
-            )
+            intToCharacter(it)
 
-            dummies.add(character);
-        }
+        }.toMutableList()
 
-        return  dummies
+    }
+    private  fun intToCharacter(int: Int): Character {
+        return Character(
+                name = "Personaje ${int}",
+                title = "Título ${int}",
+                born = "Nació en ${int}",
+                actor = "Actor ${int}",
+                quote = "Frase ${int}",
+                father = "Padre ${int}",
+                mother = "Madre ${int}",
+                spouse = "Esposa ${int}",
+                house = House(
+                        name = "Casa ${int}",
+                        region = "Region ${int}",
+                        words = "Lema ${int}"
+                )
+        )
+
     }
 }
